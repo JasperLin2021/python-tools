ac283924f9cac880d7b8364241a7de43aaed4e47
qinyuhang <tanyuhang@tuoluo.cn>
2023-11-22 18:37:01 +0800

    1

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import glob
 import os
 
 import openpyxl
 from openpyxl import load_workbook
-from openpyxl.styles import Alignment
 from openpyxl.utils import get_column_letter
-from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.workbook import Workbook
 import pandas as pd
 
-def my_fuc():
+from utils import deleteRow, unmergeCells, deleteCells, get_ad_sku_dict
+
+
+def addNewColumn():
     # 获取当前目录下的所有Excel文件
     files = [file for file in os.listdir() if file.endswith('.xlsx')]
     for file in files:
@@ -23,7 +23,7 @@ def my_fuc():
             # 设置第一行的值为"店铺"
             ws.cell(row=1, column=1).value = "店铺"
             # 获取表名的前两个字符
-            shop_name = file[:2]
+            shop_name = file[:2] + sheet
 
             # 设置余下行的值为表名的前两个字符
             i = ws.max_row
@@ -40,12 +40,14 @@ def my_fuc():
                 ws.cell(row=row, column=1).value = shop_name
             # 构建新的文件名
         new_file = file.replace('.xlsx', '-new.xlsx')
-            # 保存修改后的Excel文件为新文件
+        # 保存修改后的Excel文件为新文件
         wb.save(new_file)
 
-def hebing():
+
+def mergeFilesByWorkbookName():
     # 获取当前目录下所有以"-new.xlsx"结尾的Excel文件
     files = [file for file in os.listdir() if file.endswith('-new.xlsx')]
+    output_filename = "mergeFilesByWorkbookName.xlsx"
 
     # 字典用于存储工作簿名称和对应的工作表数据
     workbook_data = {}
@@ -54,7 +56,6 @@ def hebing():
     for file in files:
         # 加载Excel文件
         wb = load_workbook(file)
-
         # 遍历工作表
         for sheetname in wb.sheetnames:
             # 获取当前工作表
@@ -85,83 +86,21 @@ def hebing():
     del merged_wb['Sheet']
 
     # 保存合并后的Excel文件
-    merged_wb.save('merged-new-all.xlsx')
+    merged_wb.save(output_filename)
 
-def  delxiangtonghang():
-    # 获取当前目录下以"merged-new-all.xlsx"命名的Excel文件
-    file_paths = [f for f in os.listdir() if f == "merged-new-all.xlsx"]
-    for file in file_paths:
-        # 打开Excel文件
-        wb = load_workbook(file)
-        # 遍历所有工作表
-        for ws in wb.worksheets:
-            rows = list(ws.rows)
-            # 删除第二行及之后的包含"店铺"的行
-            for row in rows[1:]:
-                if "店铺" in row[0].value:
-                    ws.delete_rows(row[0].row)
-        # 保存修改
-        wb.save(file)
+    deleteRow(output_filename, 3, '店铺')
 
-def  xiaoshou_toushibiao():
+
+def salesPivotTable():
     # 读取Excel文件
-    wb = openpyxl.load_workbook('merged-new-all.xlsx')
-    ws = wb.active
-
-    # 读取数据到DataFrame
-    # data = ws.values
-    # columns = next(data)[1:]
-    # data = list(data)
-    # idx = [r[0] for r in data]
-    # data = (r[1:] for r in data)
-    # df = pd.DataFrame(data, index=idx, columns=columns)
-
-    # 读取数据到DataFrame，指定第一列为索引列
-    df = pd.read_excel('merged-new-all.xlsx', sheet_name=ws.title, index_col=0)
-
-    # 确保第一列作为列名而非索引
-    df.reset_index(inplace=True)
-
-    print(df.columns)
-
-    # 创建数据透视表
-    pivot_table = pd.pivot_table(df, values='已订购商品数量', index=['店铺', 'SKU'], aggfunc='sum')
-
-    # 创建Excel写入器
-    writer = pd.ExcelWriter('pivot_table.xlsx', engine='openpyxl')
-    writer.book = wb
-
-    # 写入数据透视表到Excel文件
-    pivot_table.to_excel(writer, sheet_name='数据透视表')
-
-    # 获取数据透视表工作表对象
-    pivot_ws = writer.sheets['数据透视表']
-
-    # 设置数据透视表布局和打印设置
-    pivot_ws.sheet_view.showGridLines = False
-    pivot_ws.sheet_properties.outlinePr.summaryBelow = True
-    pivot_ws.sheet_properties.outlinePr.summaryRight = True
-
-    # # 设置分类汇总为无
-    # pivot_ws.pivotTables.pivotTable[0].showRowLabels = False
-    #
-    # # 选择重复项目标签
-    # pivot_ws.pivotTables.pivotTable[0].rowFields[0].addItems('店铺')
-
-    # 关闭Excel写入器
-    writer.save()
-    writer.close()
-
-def  xiaoshou_toushibiao2():
-    # 读取Excel文件
-    filename = "merged-new-all.xlsx"
+    filename = "mergeFilesByWorkbookName.xlsx"
     excel_file = pd.ExcelFile(filename)
 
     # 获取所有工作簿名称
     sheet_names = excel_file.sheet_names
 
     # 创建一个新的Excel文件来保存数据透视表
-    output_filename = "pivot_tables_xiaoshou.xlsx"
+    output_filename = "Merge_SalesPivotTable.xlsx"
     writer = pd.ExcelWriter(output_filename, engine='openpyxl')
 
     # 遍历每个工作簿
@@ -184,34 +123,24 @@ def  xiaoshou_toushibiao2():
             worksheet.sheet_properties.outlinePr.summaryBelow = True
             worksheet.sheet_properties.outlinePr.summaryRight = True
 
-
-            # # 设置分类汇总为无
-            # worksheet.pivot_tables[sheet_name].showTotals = False
-            #
-            # # 设置布局和打印设置
-            # worksheet.pivot_tables[sheet_name].outline = True
-            # worksheet.pivot_tables[sheet_name].outlineData = True
-            #
-            # # 选择重复项目标签
-            # worksheet.pivot_tables[sheet_name].rowColGrandTotals = 'both'
-            # worksheet.pivot_tables[sheet_name].rowColRepeatLabels = True
-
     # 保存并关闭Excel文件
     writer.save()
     writer.close()
 
-    quxiao_hebingdanyuange(output_filename)
+    unmergeCells(output_filename)
+    deleteRow(output_filename, 2)
 
-def  shangpintuiguang_toushibiao():
+
+def productPromotionPivotTable():
     # 读取Excel文件
-    filename = "merged-new-all.xlsx"
+    filename = "mergeFilesByWorkbookName.xlsx"
     excel_file = pd.ExcelFile(filename)
 
     # 获取所有工作簿名称
     sheet_names = excel_file.sheet_names
 
     # 创建一个新的Excel文件来保存数据透视表
-    output_filename = "pivot_tables_shangpintuiguang.xlsx"
+    output_filename = "PivotTable_1_ProductPromotion.xlsx"
     writer = pd.ExcelWriter(output_filename, engine='openpyxl')
 
     # 遍历每个工作簿
@@ -221,7 +150,7 @@ def  shangpintuiguang_toushibiao():
             df = excel_file.parse(sheet_name)
 
             # 创建数据透视表
-            pivot_table = pd.pivot_table(df, values=['花费', '7天总销售额'], index=['店铺','广告SKU'], aggfunc='sum')
+            pivot_table = pd.pivot_table(df, values=['花费', '7天总销售额'], index=['店铺', '广告SKU'], aggfunc='sum')
 
             # 将数据透视表写入新的工作簿
             pivot_table.to_excel(writer, sheet_name=sheet_name, index=True, startrow=1)
@@ -234,33 +163,23 @@ def  shangpintuiguang_toushibiao():
             worksheet.sheet_properties.outlinePr.summaryBelow = True
             worksheet.sheet_properties.outlinePr.summaryRight = True
 
-            # # 设置分类汇总为无
-            # worksheet.pivot_tables[sheet_name].showTotals = False
-            #
-            # # 设置布局和打印设置
-            # worksheet.pivot_tables[sheet_name].outline = True
-            # worksheet.pivot_tables[sheet_name].outlineData = True
-            #
-            # # 选择重复项目标签
-            # worksheet.pivot_tables[sheet_name].rowColGrandTotals = 'both'
-            # worksheet.pivot_tables[sheet_name].rowColRepeatLabels = True
-
     # 保存并关闭Excel文件
     writer.save()
     writer.close()
 
-    quxiao_hebingdanyuange(output_filename)
+    unmergeCells(output_filename)
 
-def  zhanshituiguang_toushibiao():
+
+def displayPromotionPivotTable():
     # 读取Excel文件
-    filename = "merged-new-all.xlsx"
+    filename = "mergeFilesByWorkbookName.xlsx"
     excel_file = pd.ExcelFile(filename)
 
     # 获取所有工作簿名称
     sheet_names = excel_file.sheet_names
 
     # 创建一个新的Excel文件来保存数据透视表
-    output_filename = "pivot_tables_zhanshituiguang.xlsx"
+    output_filename = "PivotTable_2_DisplayPromotion.xlsx"
     writer = pd.ExcelWriter(output_filename, engine='openpyxl')
 
     # 遍历每个工作簿
@@ -270,7 +189,7 @@ def  zhanshituiguang_toushibiao():
             df = excel_file.parse(sheet_name)
 
             # 创建数据透视表
-            pivot_table = pd.pivot_table(df, values=['花费', '14天总销售额'], index=['店铺','广告SKU'], aggfunc='sum')
+            pivot_table = pd.pivot_table(df, values=['花费', '14天总销售额'], index=['店铺', '广告SKU'], aggfunc='sum')
 
             # 将数据透视表写入新的工作簿
             pivot_table.to_excel(writer, sheet_name=sheet_name, index=True, startrow=1)
@@ -283,90 +202,168 @@ def  zhanshituiguang_toushibiao():
             worksheet.sheet_properties.outlinePr.summaryBelow = True
             worksheet.sheet_properties.outlinePr.summaryRight = True
 
-            # # 设置分类汇总为无
-            # worksheet.pivot_tables[sheet_name].showTotals = False
-            #
-            # # 设置布局和打印设置
-            # worksheet.pivot_tables[sheet_name].outline = True
-            # worksheet.pivot_tables[sheet_name].outlineData = True
-            #
-            # # 选择重复项目标签
-            # worksheet.pivot_tables[sheet_name].rowColGrandTotals = 'both'
-            # worksheet.pivot_tables[sheet_name].rowColRepeatLabels = True
+    # 保存并关闭Excel文件
+    writer.save()
+    writer.close()
+
+    unmergeCells(output_filename)
+
+
+def brandPromotionPivotTable():
+    # 读取Excel文件
+    filename = "mergeFilesByWorkbookName.xlsx"
+    excel_file = pd.ExcelFile(filename)
+
+    # 获取所有工作簿名称
+    sheet_names = excel_file.sheet_names
+
+    # 创建一个新的Excel文件来保存数据透视表
+    output_filename = "PivotTable_3_BrandPromotion.xlsx"
+    writer = pd.ExcelWriter(output_filename, engine='openpyxl')
+
+    # 遍历每个工作簿
+    for sheet_name in sheet_names:
+        if sheet_name.startswith("品牌推广"):
+            # 读取工作簿数据
+            df = excel_file.parse(sheet_name)
+
+            # 创建数据透视表
+            pivot_table = pd.pivot_table(df, values=['花费', '14天总销售额'], index=['店铺', '广告活动名称'],
+                                         aggfunc='sum')
+
+            # 将数据透视表写入新的工作簿
+            pivot_table.to_excel(writer, sheet_name=sheet_name, index=True, startrow=1)
+
+            # 获取工作簿的worksheet对象
+            worksheet = writer.sheets[sheet_name]
+
+            # 设置数据透视表布局和打印设置
+            worksheet.sheet_view.showGridLines = False
+            worksheet.sheet_properties.outlinePr.summaryBelow = True
+            worksheet.sheet_properties.outlinePr.summaryRight = True
 
     # 保存并关闭Excel文件
     writer.save()
     writer.close()
 
-    quxiao_hebingdanyuange(output_filename)
+    unmergeCells(output_filename)
+    deleteRow(output_filename, 1)
 
-def quxiao_hebingdanyuange(file_path):
-    # 获取目标文件
-    # file_path = 'pivot_tables_xiaoshou.xlsx'
 
-    # 加载Excel文件
-    workbook = load_workbook(file_path)
+def modify_BrandPromotion():
+    ad_sku_dict = get_ad_sku_dict()
+    # 打开PivotTable_3_BrandPromotion.xlsx文件
+    # 打开PivotTable_3_BrandPromotion.xlsx文件
+    wb = load_workbook("PivotTable_3_BrandPromotion.xlsx")
 
-    # 遍历所有工作簿
-    for sheet_name in workbook.sheetnames:
-        sheet = workbook[sheet_name]
+    # 遍历每个工作表
+    for sheet_name in wb.sheetnames:
+        # 获取当前工作表
+        ws = wb[sheet_name]
 
-        # 获取所有合并单元格的范围和值
-        merged_cells = list(sheet.merged_cells)  # 创建副本
-        merged_values = {}
+        # 获取除第一行外的所有行数据
+        rows = list(ws.iter_rows(min_row=2))
 
-        # 遍历合并单元格
-        for merged_range in merged_cells:
-            # 获取合并单元格的范围边界
-            min_row, min_col, max_row, max_col = merged_range.min_row, merged_range.min_col, merged_range.max_row, merged_range.max_col
+        # 遍历每一行数据，向下新增两行
+        for row in rows:
+            # 获取当前行的行号
+            row_number = row[0].row
+            date = row[0].value
+            name = row[1].value
+            total_sales = row[2].value
+            cost = row[3].value
 
-            # 获取合并单元格的值
-            merged_value = sheet.cell(row=min_row, column=min_col).value
+            for i in ad_sku_dict:
+                if name == i:
+                    add_row = len(ad_sku_dict[name])
 
-            # 保存合并单元格的值和范围
-            merged_values[merged_range.coord] = {
-                'value': merged_value,
-                'range': (min_row, min_col, max_row, max_col)
-            }
+                    ws.insert_rows(row_number + 1, amount=add_row)
 
-            # 取消合并单元格
-            sheet.unmerge_cells(str(merged_range))
+                    for i in range(1, add_row + 1):
+                        # print(i)
+                        ws.cell(row=row_number + i, column=1, value=date)
+                        ws.cell(row=row_number + i, column=2, value=ad_sku_dict[name][i - 1])
+                        ws.cell(row=row_number + i, column=3, value=total_sales / add_row)
+                        ws.cell(row=row_number + i, column=4, value=cost / add_row)
 
-        # 恢复取消合并的单元格的值
-        for coord, data in merged_values.items():
-            merged_value = data['value']
-            min_row, min_col, max_row, max_col = data['range']
-            for row in range(min_row, max_row + 1):
-                for col in range(min_col, max_col + 1):
-                    sheet.cell(row=row, column=col).value = merged_value
-
+            # 删除原始行
+            ws.delete_rows(row_number, amount=1)
     # 保存修改后的Excel文件
-    workbook.save(file_path)
+    wb.save("PivotTable_3_BrandPromotion.xlsx")
+
+    # 打印广告活动名称和对应的广告SKU值
+    # for ad_name, ad_skus in ad_sku_dict.items():
+    #     print(f"广告活动名称: {ad_name}, 广告SKU: {ad_skus}")
 
 
-def huizong():
-    # 设置文件夹路径和新文件名
-    folder_path = './'  # 设置包含要合并的Excel文件的文件夹路径
-    output_file = '汇总.xlsx'  # 设置输出文件名
+def mergePivotTable():
+    # 获取当前目录下所有以"-new.xlsx"结尾的Excel文件
+    files = [file for file in os.listdir() if file.startswith('PivotTable_')]
+    output_filename = "Merge_PromotionPivotTable.xlsx"
+    # 字典用于存储工作簿名称和对应的工作表数据
+    workbook_data = {}
 
-    # 获取文件夹中以"pivot_"开头的所有Excel文件
-    files = [file for file in os.listdir(folder_path) if file.startswith('pivot_') and file.endswith('.xlsx')]
+    # 遍历Excel文件
+    for file in files:
+        # 加载Excel文件
+        wb = load_workbook(file)
+
+        # 遍历工作表
+        for sheetname in wb.sheetnames:
+            # 获取当前工作表
+            ws = wb[sheetname]
+
+            # 如果工作簿名称已存在于字典中，则将当前工作表数据追加到已存在的列表中
+            if sheetname[4:] in workbook_data:
+                workbook_data[sheetname[4:]].extend(ws.iter_rows(values_only=True))
+            else:
+                # 如果工作簿名称不存在于字典中，则将当前工作表数据添加到字典中
+                workbook_data[sheetname[4:]] = list(ws.iter_rows(values_only=True))
+
+    # 创建新的Workbook对象
+    merged_wb = Workbook()
+
+    # 遍历工作簿数据字典
+    for sheetname, data in workbook_data.items():
+        # 创建新的工作表
+        ws = merged_wb.create_sheet(title=sheetname)
+
+        # 写入数据到工作表
+        for row_index, row_data in enumerate(data):
+            for col_index, cell_value in enumerate(row_data):
+                column_letter = get_column_letter(col_index + 1)
+                ws[column_letter + str(row_index + 1)].value = cell_value
+
+    # 删除默认创建的Sheet
+    del merged_wb['Sheet']
+
+    # 保存合并后的Excel文件
+    merged_wb.save(output_filename)
+
+    deleteRow(output_filename, 1)
+    deleteRow(output_filename, 3, '店铺')
+
+
+def summary():
+    # 获取文件夹中以"Merge_"开头的所有Excel文件
+    files = [file for file in os.listdir() if file.startswith('Merge_')]
+    output_filename = "汇总.xlsx"
 
     # 创建一个空的DataFrame用于存储合并后的数据
     merged_data = pd.DataFrame()
 
     # 遍历每个Excel文件并将所有工作簿横向合并
     for file in files:
-        file_path = os.path.join(folder_path, file)
+        # file_path = os.path.join(folder_path, file)
 
         # 读取Excel文件中的所有工作簿
-        xl = pd.ExcelFile(file_path)
+        xl = pd.ExcelFile(file)
         sheet_names = xl.sheet_names
 
         # 遍历每个工作簿并将其横向合并到merged_data中
         for sheet_name in sheet_names:
             # 读取当前工作簿的数据
-            df = pd.read_excel(file_path, sheet_name=sheet_name)
+            df = pd.read_excel(file, sheet_name=sheet_name)
 
             # 在最右侧新增两个空白列
             df_with_columns = df.assign(NewColumn1="", NewColumn2="")
@@ -375,16 +372,21 @@ def huizong():
             merged_data = pd.concat([merged_data, df_with_columns], axis=1)
 
     # 将合并后的数据写入新的Excel文件
-    merged_data.to_excel(output_file, index=False)
+    merged_data.to_excel(output_filename, index=False)
+
+    # 删除指定单元格
+    deleteCells(output_filename, "NewColumn")
+
 
 if __name__ == '__main__':
     print("这个脚本正在直接运行。")
 
-    # my_fuc()
-    # hebing()
-    # delxiangtonghang()
-    # xiaoshou_toushibiao()
-    xiaoshou_toushibiao2()
-    shangpintuiguang_toushibiao()
-    zhanshituiguang_toushibiao()
-    huizong()
+    addNewColumn()
+    mergeFilesByWorkbookName()
+    salesPivotTable()
+    productPromotionPivotTable()
+    displayPromotionPivotTable()
+    brandPromotionPivotTable()
+    modify_BrandPromotion()
+    mergePivotTable()
+    summary()
---
+++ utils.py
@@ -0,0 +1,113 @@
+import openpyxl
+from openpyxl.reader.excel import load_workbook
+
+
+def deleteRow(file, type, keyword=None):
+    wb = load_workbook(file)
+
+    # 删除空白行
+    if type == 1:
+        # 遍历所有工作表
+        for ws in wb.worksheets:
+            rows = list(ws.rows)
+            for row in reversed(rows):
+                if row[0].value is None:
+                    ws.delete_rows(row[0].row)
+    # 删除第一行
+    elif type == 2:
+        # 遍历所有工作表
+        for ws in wb.worksheets:
+            ws.delete_rows(1)
+
+    # 删除第二行及之后的包含"店铺"的行
+    elif type == 3:
+        for ws in wb.worksheets:
+            rows = list(ws.rows)
+            for row in rows[1:]:
+                if keyword in row[0].value:
+                    ws.delete_rows(row[0].row)
+    # 保存修改
+    wb.save(file)
+
+
+def unmergeCells(file):
+    # 加载Excel文件
+    workbook = load_workbook(file)
+
+    # 遍历所有工作簿
+    for sheet_name in workbook.sheetnames:
+        sheet = workbook[sheet_name]
+
+        # 获取所有合并单元格的范围和值
+        merged_cells = list(sheet.merged_cells)  # 创建副本
+        merged_values = {}
+
+        # 遍历合并单元格
+        for merged_range in merged_cells:
+            # 获取合并单元格的范围边界
+            min_row, min_col, max_row, max_col = merged_range.min_row, merged_range.min_col, merged_range.max_row, merged_range.max_col
+
+            # 获取合并单元格的值
+            merged_value = sheet.cell(row=min_row, column=min_col).value
+
+            # 保存合并单元格的值和范围
+            merged_values[merged_range.coord] = {
+                'value': merged_value,
+                'range': (min_row, min_col, max_row, max_col)
+            }
+
+            # 取消合并单元格
+            sheet.unmerge_cells(str(merged_range))
+
+        # 恢复取消合并的单元格的值
+        for coord, data in merged_values.items():
+            merged_value = data['value']
+            min_row, min_col, max_row, max_col = data['range']
+            for row in range(min_row, max_row + 1):
+                for col in range(min_col, max_col + 1):
+                    sheet.cell(row=row, column=col).value = merged_value
+
+    # 保存修改后的Excel文件
+    workbook.save(file)
+
+
+def deleteCells(file, keyword):
+    # 加载Excel文件
+    wb = load_workbook(file)
+
+    # 遍历所有工作表
+    for ws in wb.worksheets:
+        # 遍历每个单元格
+        for row in ws.iter_rows():
+            for cell in row:
+                # 检查单元格内容是否包含"NewColumn"
+                if cell.value and "NewColumn" in str(cell.value):
+                    # 将内容设置为空
+                    cell.value = None
+                    cell.font = None
+                    cell.border = None
+                    cell.protection = None
+                    cell.alignment = None
+
+    # 保存修改
+    wb.save(file)
+
+
+def get_ad_sku_dict():
+    ad_sku_dict = {}
+
+    # 打开品牌广告明细sku.xlsx文件
+    wb = openpyxl.load_workbook("品牌广告明细sku.xlsx")
+    ws = wb.active
+
+    # 遍历每行数据，构建字典
+    for row in ws.iter_rows(min_row=2, values_only=True):
+        ad_name = row[0]
+        ad_sku = row[1]
+
+        if ad_name not in ad_sku_dict:
+            ad_sku_dict[ad_name] = [ad_sku]
+        else:
+            ad_sku_dict[ad_name].append(ad_sku)
+
+    return ad_sku_dict
